60a49c1f03a5b09112d7442e1015adb8ccacd476
Pratik Koli <pratikkoli0403@gmail.com>
2025-12-30 09:33:26 +0000

    Add automated health checks with alerts

--- scripts/system_health.sh
+++ scripts/system_health.sh
@@ -8,14 +8,32 @@ echo "CPU Load:"
 uptime
 echo
 
-echo "Memory Usage:"
-free -h
+MEM_FREE=$(free | awk '/Mem/ {print $4}')
+
+echo "Memory Check:"
+if [ "$MEM_FREE" -lt 200000 ]; then
+    echo "Low memory WARNING ❌"
+else
+    echo "Memory is sufficient ✅"
+fi
 echo
 
-echo "Disk Usage:"
-df -h
+
+DISK_USAGE=$(df / | tail -1 | awk '{print $5}' | sed 's/%//')
+echo "Disk Usage Check:"
+if [ "$DISK_USAGE" -gt 80 ]; then
+    echo "WARNING: Disk usage is above 80% ❌"
+else
+    echo "Disk usage is normal ✅"
+fi
 echo
 
-echo "Apache status:"
-systemctl is-active apache2
+
+echo "Apache Service Status:"
+if systemctl is-active apache2 >/dev/null; then
+    echo "Apache is RUNNING ✅"
+else
+    echo "Apache is NOT RUNNING ❌"
+fi
+echo
 
