bfa9114a99b29e11b2b38106e9e12f1d654c76d5
Pratik Koli <pratikkoli0403@gmail.com>
2025-12-30 09:48:24 +0000

    Fix incomplete health check logic

--- scripts/system_health.sh
+++ scripts/system_health.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 
-echo "======== SYSTEM HEALTH REPORT ====="
+echo "===== SYSTEM HEALTH REPORT ====="
 date
 echo
 
 echo "CPU Load:"
 uptime
+echo
+
+echo "Memory Usage:"
+free -h
+echo
+
+echo "Disk Usage:"
+df -h
+echo
+
+echo "Apache Service Status:"
+if systemctl is-active apache2 >/dev/null; then
+    echo "Apache is RUNNING ✅"
+else
+    echo "Apache is NOT RUNNING ❌"
+fi
+echo
+
+DISK_USAGE=$(df / | tail -1 | awk '{print $5}' | sed 's/%//')
+
+echo "Disk Usage Check:"
+if [ "$DISK_USAGE" -gt 80 ]; then
+    echo "WARNING: Disk usage is above 80% ❌"
+else
+    echo "Disk usage is normal ✅"
+fi
 echo
 
 MEM_FREE=$(free | awk '/Mem/ {print $4}')
@@ -16,24 +42,3 @@ if [ "$MEM_FREE" -lt 200000 ]; then
 else
     echo "Memory is sufficient ✅"
 fi
-echo
-
-
-DISK_USAGE=$(df / | tail -1 | awk '{print $5}' | sed 's/%//')
-echo "Disk Usage Check:"
-if [ "$DISK_USAGE" -gt 80 ]; then
-    echo "WARNING: Disk usage is above 80% ❌"
-else
-    echo "Disk usage is normal ✅"
-fi
-echo
-
-
-echo "Apache Service Status:"
-if systemctl is-active apache2 >/dev/null; then
-    echo "Apache is RUNNING ✅"
-else
-    echo "Apache is NOT RUNNING ❌"
-fi
-echo
-
